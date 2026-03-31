c3737a11b8fe777091b2273e40dfefe831365402
DaniGRM <danigraneroaudio@gmail.com>
2026-03-31 10:26:37 +0200

    created card screen and loader blades

---
+++ resources/views/bmo/loader.blade.php
@@ -0,0 +1,11 @@
+<div id="bmo-loader" class="bmo-loader">
+    <div class="loader-content">
+
+        <div class="spinner"></div>
+
+        <div class="loader-text">
+            Procesando...
+        </div>
+
+    </div>
+</div>

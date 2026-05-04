dd34b678e988865811619b3046fcaae3090a52a3
DaniGRM <danigraneroaudio@gmail.com>
2026-05-04 21:08:19 +0200

    modify schedule

--- routes/console.php
+++ routes/console.php
@@ -7,3 +7,6 @@ use Illuminate\Support\Facades\Schedule;
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+$schedule->command('tasks:generate')->dailyAt('00:01')->withoutOverlapping();
+$schedule->command('tasks:calculate-bonus')->dailyAt('01:00')->withoutOverlapping();

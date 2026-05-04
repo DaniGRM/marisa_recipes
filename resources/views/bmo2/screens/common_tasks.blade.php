be5f15a53d72f07b4ca2bcd286884cf81b984da7
DaniGRM <danigraneroaudio@gmail.com>
2026-05-04 21:23:45 +0200

    fix common filter

--- resources/views/bmo2/screens/common_tasks.blade.php
+++ resources/views/bmo2/screens/common_tasks.blade.php
@@ -3,8 +3,8 @@
     <div class="row " style="padding-bottom: 100px;">
 
         @foreach($commonTasks as $task)
-            <div class="col-6 pt-3">
-            <div class="task common-task" onclick='bmoApp.completeTaskConfirm({{ $task->id }}, {{ json_encode($task->description) }}, true)'  data-room="{{ $task->room->name ?? '' }}">
+            <div class="col-6 pt-3" data-room="{{ $task->room->name ?? '' }}">
+            <div class="task common-task" onclick='bmoApp.completeTaskConfirm({{ $task->id }}, {{ json_encode($task->description) }}, true)'  >
 
                 <span class="common-task-title">
                     {{ $task->name }}
